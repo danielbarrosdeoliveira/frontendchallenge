2eb8f012b38c4ee9e89c4523ca7f72d446c4048a
Daniel Barros de Oliveira <danielbarrosdeoliveira@outlook.com>
2021-08-22 13:44:16 -0300

    add normalize to App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,11 @@
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import 'normalize.css';
 
-#nav {
-  padding: 30px;
-}
+export default defineComponent({});
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
