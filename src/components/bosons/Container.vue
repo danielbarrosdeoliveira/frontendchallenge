fafa039d7ccbbc2e41e0e56950688b419f4dee9b
Daniel Barros de Oliveira <danielbarrosdeoliveira@outlook.com>
2021-08-23 11:29:20 -0300

    container boson

---
+++ src/components/bosons/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Container',
+});
+</script>
+
+<style scoped>
+.container {
+  width: 85%;
+  max-width: 1180px;
+  margin: 0 auto;
+}
+</style>
